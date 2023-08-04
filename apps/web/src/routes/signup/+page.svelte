3344d280f66e9680088c68e644364baeaa79db8a
Martin Sonnberger <martin@sonnberger.email>
2023-08-04 16:01:20 +0200

    feat(auth): style signup page

--- apps/web/src/routes/signup/+page.svelte
+++ apps/web/src/routes/signup/+page.svelte
@@ -1,13 +1,69 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
+	import { Button } from '$components/button';
+	import { Icons } from '$components/icons';
+	import { Input } from '$components/input';
 </script>
 
-<h1>Sign up</h1>
-<form method="post" use:enhance>
-	<label for="email">Email</label>
-	<input name="email" id="email" type="email" /><br />
-	<label for="password">Password</label>
-	<input type="password" name="password" id="password" /><br />
-	<input type="submit" />
-</form>
-<a href="/auth/login">Sign in</a>
+<div class="min-h-screen grid grid-cols-1 lg:grid-cols-2 items-center">
+	<div class="relative hidden lg:block bg-slate-900 self-stretch">
+		<div class="absolute left-12 right-32 top-12">
+			<p class="text-white text-xl">Fontesio</p>
+			<p class="mt-36 text-6xl text-white font-bold">All of your references in one place.</p>
+		</div>
+	</div>
+
+	<div class="w-full max-w-md mx-auto mt-16">
+		<div class="space-y-2">
+			<h1 class="text-3xl font-semibold">Create an account</h1>
+			<p class="text-sm">Use a provider or enter your email below to create your account</p>
+		</div>
+
+		<div class="grid gap-4">
+			<div class="grid grid-cols-2 gap-4 mt-6">
+				<Button variant="outline">
+					<Icons.Apple class="mr-2 h-4 w-4 -translate-y-[1px]" />
+					Sign in with Apple
+				</Button>
+				<Button variant="outline" href="/auth/login/google">
+					<Icons.Google class="mr-2 h-4 w-4" />
+					Sign in with Google
+				</Button>
+			</div>
+			<div class="relative">
+				<div class="absolute inset-0 flex items-center">
+					<span class="w-full border-t" />
+				</div>
+				<div class="relative flex justify-center text-xs uppercase">
+					<span class="bg-background px-2 text-muted-foreground"> Or continue with </span>
+				</div>
+			</div>
+			<form id="email-password-form" class="grid gap-4" method="post" use:enhance>
+				<Input
+					label="Email address"
+					placeholder="john.doe@example.com"
+					type="email"
+					name="email"
+					id="email"
+				/>
+				<Input
+					label="Password"
+					placeholder="•••••••••••••"
+					type="password"
+					name="password"
+					id="password"
+				/>
+				<ul class="list-disc list-inside text-sm -mt-2 mb-2 ml-2">
+					<li>Minimum 8 characters long</li>
+					<li>Mix of uppercase and lowercase letters</li>
+					<li>Contains at least one number</li>
+				</ul>
+			</form>
+
+			<Button form="email-password-form" type="submit" class="w-full">Sign up for free</Button>
+		</div>
+		<div class="mt-8 text-center">
+			<Button href="/auth/login" variant="link">Already have an account?</Button>
+		</div>
+	</div>
+</div>
