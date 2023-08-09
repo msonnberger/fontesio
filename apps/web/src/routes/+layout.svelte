2461e81b1a54b07d11060b59ba11c35657e421d2
Martin Sonnberger <martin@sonnberger.email>
2023-08-09 14:30:05 +0200

    feat: add favicons

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script>
 	import '../app.postcss';
+	import icon from '$lib/assets/logo/icon.svg';
+	import apple_touch_icon from '$lib/assets/logo/apple-touch-icon.png';
 </script>
+
+<svelte:head>
+	<link rel="icon" type="image/svg+xml" href={icon} />
+	<link rel="apple-touch-icon" href={apple_touch_icon} />
+</svelte:head>
 
 <slot />
