fbd0facbdf92cb064e7b33bf1e7700ecd3035cca
Martin Sonnberger <martin@sonnberger.email>
2023-08-12 10:17:02 +0200

    feat(home): add sidebar

--- apps/web/src/routes/(app)/+page.svelte
+++ apps/web/src/routes/(app)/+page.svelte
@@ -1,10 +1,1 @@
-<script lang="ts">
-	export let data;
-</script>
-
-<h1>Welcome</h1>
-<p>User id: {data.user.userId}</p>
-<p>Email: {data.user.email}</p>
-<p>Verified: {data.user.email_verified}</p>
-
-<a href="/logout">Sign out</a>
+<h1>All references</h1>
