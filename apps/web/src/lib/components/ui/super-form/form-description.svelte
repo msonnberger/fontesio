c0f5c924a3b883c8247bbc2db79bcc31837a8a16
Martin Sonnberger <martin@sonnberger.email>
2023-08-20 09:28:31 +0200

    feat(ui): add super-form component

---
+++ apps/web/src/lib/components/ui/super-form/form-description.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+	import { getContext } from 'svelte';
+	import type { FormFieldContext } from './types';
+	import { FORM_FIELD_CONTEXT } from '.';
+	let className: string | undefined | null = undefined;
+	export { className as class };
+
+	const { formDescriptionId } = getContext<FormFieldContext>(FORM_FIELD_CONTEXT);
+</script>
+
+<p
+	class={cn('text-[0.8rem] text-muted-foreground', className)}
+	{...$$restProps}
+	id={formDescriptionId}
+>
+	<slot />
+</p>
