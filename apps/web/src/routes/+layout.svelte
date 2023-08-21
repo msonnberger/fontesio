267456359bc116486ceac97c9a1593c023dc6781
Martin Sonnberger <martin@sonnberger.email>
2023-08-21 11:36:21 +0200

    feat(perf): prefetch font

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -7,6 +7,7 @@
 <svelte:head>
 	<link rel="icon" type="image/svg+xml" href={icon} />
 	<link rel="apple-touch-icon" href={apple_touch_icon} />
+	<link rel="prefetch" href="/fonts/Inter-roman.var.woff2" />
 </svelte:head>
 
 <slot />
