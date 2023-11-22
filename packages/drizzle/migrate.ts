d8024ee648106b037168e6634332cd039e176b94
Martin Sonnberger <martin@sonnberger.email>
2023-11-22 21:07:58 +0100

    feat(drizzle): add drizzle package

---
+++ packages/drizzle/migrate.ts
@@ -0,0 +1,17 @@
+import { env } from '@fontesio/env/server';
+import { drizzle } from 'drizzle-orm/postgres-js';
+import { migrate } from 'drizzle-orm/postgres-js/migrator';
+import postgres from 'postgres';
+
+const migration_client = postgres(env.DATABASE_URL, { max: 1 });
+
+migrate(drizzle(migration_client), { migrationsFolder: './migrations' })
+	.then(() => {
+		console.log('Migrations applied');
+		process.exit(0);
+	})
+	.catch((err) => {
+		console.error(err);
+		console.log('Failed to apply migrations');
+		process.exit(1);
+	});
