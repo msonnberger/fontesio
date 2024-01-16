e9b53b57a98088644ca55c2f9522fcd452ff354e
Martin Sonnberger <martin@sonnberger.email>
2024-01-16 20:21:09 +0100

    feat(add-resource): refactor

---
+++ apps/web/src/lib/features/add-resource/components/open-sheet-menu.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import PlusCircle from '~icons/lucide/plus-circle';
+	import { Button } from '@fontesio/ui/primitives/button';
+	import * as DropdownMenu from '@fontesio/ui/primitives/dropdown-menu';
+	import { SheetTrigger } from '@fontesio/ui/primitives/sheet';
+	import { unslugify } from '$lib/utils/unslugify';
+	import type { CslType } from '@fontesio/citations/types';
+	import { csl_types } from '@fontesio/citations/csl-json-schema';
+
+	export let set_selected: (value: string) => void;
+
+	const shortcut_types: CslType[] = [
+		'book',
+		'article-journal',
+		'article-newspaper',
+		'document',
+		'chapter',
+	];
+
+	const more_types = csl_types.filter((type) => !shortcut_types.includes(type));
+</script>
+
+<DropdownMenu.Root>
+	<DropdownMenu.Trigger asChild let:builder>
+		<Button builders={[builder]} class="inline-flex items-center">
+			<PlusCircle class="mr-2" /> Add resource
+		</Button>
+	</DropdownMenu.Trigger>
+	<DropdownMenu.Content>
+		<DropdownMenu.Group>
+			{#each shortcut_types as type}
+				<DropdownMenu.Item class="p-0">
+					<SheetTrigger class="px-2 py-1 w-full text-left" on:click={() => set_selected(type)}>
+						{unslugify(type)}
+					</SheetTrigger>
+				</DropdownMenu.Item>
+			{/each}
+		</DropdownMenu.Group>
+		<DropdownMenu.Separator />
+		<DropdownMenu.Group>
+			<DropdownMenu.Sub>
+				<DropdownMenu.SubTrigger class="py-1">More</DropdownMenu.SubTrigger>
+				<DropdownMenu.SubContent class="h-80 w-max overflow-y-scroll">
+					{#each more_types as type}
+						<DropdownMenu.Item class="p-0">
+							<SheetTrigger class="px-2 py-1 w-full text-left" on:click={() => set_selected(type)}>
+								{unslugify(type)}
+							</SheetTrigger>
+						</DropdownMenu.Item>
+					{/each}
+				</DropdownMenu.SubContent>
+			</DropdownMenu.Sub>
+		</DropdownMenu.Group>
+	</DropdownMenu.Content>
+</DropdownMenu.Root>
---
+++ apps/web/src/lib/features/add-resource/components/sheet.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import * as Sheet from '@fontesio/ui/primitives/sheet';
+	import Form from './Form.svelte';
+	import { page } from '$app/stores';
+	import type { CslType } from '@fontesio/citations/types';
+	import OpenSheetMenu from '$lib/features/add-resource/components/open-sheet-menu.svelte';
+
+	let sheet_open = false;
+	let selected_type: CslType;
+
+	function set_selected(type: string) {
+		selected_type = type as CslType;
+	}
+</script>
+
+<Sheet.Root bind:open={sheet_open}>
+	<OpenSheetMenu {set_selected} />
+	<Sheet.Content side="right" class="min-w-[40rem]">
+		<Sheet.Header>
+			<Sheet.Title>Add resource</Sheet.Title>
+			<Sheet.Description>Add a new resource to your collection.</Sheet.Description>
+		</Sheet.Header>
+		<Form initial_type={selected_type} bind:sheet_open form={$page.data.form} />
+	</Sheet.Content>
+</Sheet.Root>
