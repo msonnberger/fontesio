786267b1845d87ae59634cdb8f767252106f7dc5
Martin Sonnberger <martin@sonnberger.email>
2024-02-20 14:54:12 +0100

    update shadcn form

--- packages/ui/primitives/form/form-button.svelte
+++ packages/ui/primitives/form/form-button.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
 	import * as Button from '../button';
 	type $$Props = Button.Props;
-	type $$Events = Button.Events;
 </script>
 
-<Button.Root type="submit" {...$$restProps} on:click on:keydown>
+<Button.Root type="submit" {...$$restProps}>
 	<slot />
 </Button.Root>
